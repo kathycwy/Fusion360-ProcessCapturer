8ca74c40b74634a92d8e5ebb56214d2e0973d5ef
Wing Yin Chan <wingyin97606@gmail.com>
2021-11-23 19:53:34 +0100

    Initial commit

---
+++ ProcessToVideo/ProcessToVideo.py
@@ -0,0 +1,95 @@
+#Author-Wingyin Chan
+#Description-Convert your creation process into video
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+import os
+
+# Global list to keep all event handlers in scope
+handlers = []
+
+def run(context):
+    try:
+        global app, ui
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+
+        # Get the CommandDefinitions collection
+        cmdDefs = ui.commandDefinitions
+        
+        # Create a command definition and add a button to the Add-ins panel.
+        cmdDef = ui.commandDefinitions.addButtonDefinition('processToVideoAddIn', 
+                                                            'ProcessToVideo', 
+                                                            'Convert your creation process into video',
+                                                            '')        
+        addInsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        addInsButton = addInsPanel.controls.addCommand(cmdDef)
+        
+        # Connect to the command created event.
+        onCommandCreated = CommandCreatedEventHandler()
+        cmdDef.commandCreated.add(onCommandCreated)
+        handlers.append(onCommandCreated)
+        
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+
+# Event handler for the commandCreated event
+class CommandCreatedEventHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.CommandCreatedEventArgs.cast(args)
+        
+        # Get the command
+        cmd = eventArgs.command
+
+        # Get the CommandInputs collection to create new command inputs          
+        inputs = cmd.commandInputs
+
+        # Get user-defined n
+        durationN = inputs.addIntegerSpinnerCommandInput('durationN', 'Input n', 1, 10, 1, 1)
+
+        targetFolder = inputs.addTextBoxCommandInput('targetFolder', 'Target folder', '', 1, False)
+
+        # Connect to the execute event
+        onExecute = CommandExecuteHandler()
+        cmd.execute.add(onExecute)
+        handlers.append(onExecute)
+
+
+# Event handler for the execute event
+class CommandExecuteHandler(adsk.core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.CommandEventArgs.cast(args)
+
+        # Get the values from the command inputs 
+        inputs = eventArgs.command.commandInputs
+
+        # Get the value of n
+        n = int(inputs.itemById('durationN').value)
+        targetFolder = str(inputs.itemById('targetFolder'))
+
+        # ui.messageBox("Save image to ", targetFolder)
+
+        # Save image
+        filename = os.path.join("/Users/wingyin/Downloads", "frame")
+        app.activeViewport.saveAsImageFile(filename, 0, 0)  
+
+
+def stop(context):
+    try:
+        addInsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        addInsButton = addInsPanel.controls.itemById('processToVideoAddIn')       
+        if addInsButton:
+            addInsButton.deleteMe()
+        
+        cmdDef = ui.commandDefinitions.itemById('processToVideoAddIn')
+        if cmdDef:
+            cmdDef.deleteMe()
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
